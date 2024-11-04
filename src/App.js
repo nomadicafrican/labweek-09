0ecd5b2b26474f3a566f57152f43e27267daf04b
Ammar <ammar.kusow2@gmail.com>
2024-11-03 21:03:45 -0500

    repo init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="app-container">
+      <img src={logo} className="app-logo" alt="logo" />
+      <h1>Welcome to Fullstack Development - I</h1>
+      <h2>React JS Programming Week09 Lab exercise</h2>
+      <p>101388320</p>
+      <p>Ammar Ali</p>
+      <p>George Brown College, Toronto</p>
+    </div>
+  );
+}
+
+export default App;
